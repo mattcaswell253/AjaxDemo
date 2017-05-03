8db71337e3f7f8a38e71a4cb015a2bc0b1148fad
Kory Skarbek and Matt Caswell <kory+matt@>
2017-05-03 09:05:14 -0700

    stuff

---
+++ src/AjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult HelloAjax()
+        {
+            return Content("Hello from the controller!", "text/plain");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sum(int firstNumber, int secondNumber)
+        {
+            return Content((firstNumber + secondNumber).ToString(), "text/plain");
+        }
+        public IActionResult DisplayObject()
+        {
+            Destination destination = new Destination("Tokyo", "Japan", 1);
+            return Json(destination);
+        }
+        public IActionResult DisplayViewWithAjax()
+        {
+            return View();
+        }
+    }
+}
